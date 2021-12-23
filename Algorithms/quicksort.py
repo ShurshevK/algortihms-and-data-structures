95f98a15d59109d2b83949b894fb3af46e9fcf04
ShurshevK <shurshevkirill@gmail.com>
2021-12-23 21:30:07 +0100

    init

---
+++ Algorithms/quicksort.py
@@ -0,0 +1,21 @@
+numbers = [9,5,3,4,5,7,7,8]
+
+def quicksort (values):
+    if len(values) <= 1:
+        return values
+
+    less_than_pivot = []
+
+    greater_than_pivot = []
+
+    pivot = values[0]
+
+    for value in values[1:]:
+        if value <= pivot:
+            less_than_pivot.append(value)
+        else:
+            greater_than_pivot.append(value)
+    print("%15s %1s %-15s" % (less_than_pivot, pivot, greater_than_pivot))
+    return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+
+print(quicksort(numbers))
